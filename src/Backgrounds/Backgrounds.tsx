e4bfcbeca979fa610df88fa07810611f72e1665d
H4ckW1s3r <clementix01@gmail.com>
2023-09-17 21:52:40 +0200

    Deleted Canvas

--- src/Backgrounds/Backgrounds.tsx
+++ src/Backgrounds/Backgrounds.tsx
@@ -1,11 +1,9 @@
-import { Canvas } from "./Canvas/Canvas";
 import { Stars } from "./Stars/Stars";
 
 export function Backgrounds(){
     return (
         <>
             <Stars></Stars>
-            <Canvas></Canvas>
         </>
     );
 }
